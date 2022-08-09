2d2fb7847be1910a109c33e393f42de0f25fe038
mich <michelle.adame@cceo.com.mx>
2022-08-09 18:36:36 -0500

    Fundamentos Vuex hasta lección Parámetros en Vuex

---
+++ src/components/BtnDisminuir.vue
@@ -0,0 +1,13 @@
+<template>
+   <button @click="accionDisminuir(50)">Disminuir</button> <!--Boton que disminuye el contador mediante la accionDisminuir enviando un parámetro-->
+</template>
+
+<script>
+import {mapActions} from 'vuex'/*importamos de vuex las acciones  */
+export default{
+    methods:{
+        ...mapActions(['accionDisminuir'])/*mapeo de accion que acciona la mutacion de Disminuir */
+    }
+}
+</script>
+
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,25 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    contador:200
   },
   getters: {
   },
   mutations: {
+    incrementar(state){
+      state.contador = state.contador + 10
+    },
+    disminuir(state,payload){ /*disminuye el contador recibiendo el parametro payload desde accionDisminuir*/
+      state.contador = state.contador - payload
+    }
   },
   actions: {
+    accionIncrementar({commit}){
+      commit('incrementar')
+    },
+    accionDisminuir({commit}, numero){ /*Accion que activa la mutación disminuir, recibiendo un parámetro desde el componente BtnDisminuir*/
+      commit('disminuir', numero)
+    }
   },
   modules: {
   }
